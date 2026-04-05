0fe02a1ae4a58f50a14be63223955a80c2dc34b2
goshanchic <91145393+goshanchic@users.noreply.github.com>
2026-04-05 13:41:07 +0300

    laba2

---
+++ laba2/1.cpp
@@ -0,0 +1,90 @@
+﻿#include <iostream>
+#include <thread>
+#include <vector>
+#include <semaphore> 
+#include <mutex>
+#include <chrono>
+#include <random>
+#include <atomic>
+
+using namespace std; 
+
+// Ресурсы порта
+counting_semaphore<10> cranes(5); // 5 погрузчиков
+mutex output_mtx;                 // для синхронизации 
+atomic<int> waiting_count(0);     // очередь счётчик грузовиков
+atomic<int> loaded_count(0);      // Счетчик уже загруженных г
+bool reserve_crane_deployed = false; // флаг активации  крана
+
+void truck(int id) {
+    // прибытие грузовика
+    {
+        lock_guard<mutex> lock(output_mtx);
+        waiting_count++;
+        cout << "[Порт] Грузовик #" << id << " прибыл. Очередь: " << waiting_count << endl;
+    }
+
+    
+    //  в очереди более 5 грузовиков, запуск доп кран 
+    if (waiting_count > 5 && !reserve_crane_deployed) {
+        lock_guard<mutex> lock(output_mtx);
+        if (!reserve_crane_deployed) {
+            cranes.release(); // +1 кран
+            reserve_crane_deployed = true;
+            cout << ">>> ВНИМАНИЕ: Очередь велика. Запущен резервный кран!" << endl;
+        }
+    }
+
+    // Ожидание и захват крана
+    cranes.acquire(); // поток занимает кран 
+    waiting_count--;  // выход из очереди ожидания
+
+    // процесс погрузки
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<> dis(3, 6); // 3-6 секунд время загрузки
+
+    int duration = dis(gen);
+    //  загружено менее 3 грузовиков, время сокращается 
+    if (loaded_count < 3) {
+        duration = 1; // мин возможное время для ускорения
+        {
+            lock_guard<mutex> lock(output_mtx);
+            cout << "[!] АВАРИЙНЫЙ РЕЖИМ: Грузовик #" << id << " загружается ускоренно (1 сек)." << endl;
+        }
+    }
+    else {
+        {
+            lock_guard<mutex> lock(output_mtx);
+            cout << "Грузовик #" << id << " начал загрузку (план: " << duration << " сек)." << endl;
+        }
+    }
+
+    
+    this_thread::sleep_for(chrono::seconds(duration));
+
+    {
+        lock_guard<mutex> lock(output_mtx);
+        loaded_count++;
+        cout << "Грузовик #" << id << " загружен. Всего выехало: " << loaded_count << endl;
+    }
+
+    cranes.release(); // освобождение крана для следующего грузовика
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    vector<thread> fleet;
+    for (int i = 1; i <= 10; ++i) {
+        fleet.push_back(thread(truck, i));
+        // для наглядности
+        this_thread::sleep_for(chrono::milliseconds(100));
+    }
+    for (auto& t : fleet) {
+        t.join();
+    }
+
+    cout << "--- Работа порта завершена. Все 10 грузовиков обработаны. ---" << endl;
+
+    return 0;
+}
---
+++ laba2/2.cpp
@@ -0,0 +1,128 @@
+﻿#include <iostream>
+#include <thread>
+#include <vector>
+#include <queue>
+#include <semaphore>
+#include <mutex>
+#include <chrono>
+#include <atomic>
+#include <string>
+
+using namespace std;
+
+struct VideoFrame {
+    int cameraId;
+    int frameNumber;
+    bool isImportant; 
+
+    bool operator<(const VideoFrame& other) const {
+        if (isImportant != other.isImportant) {
+            return !isImportant; 
+        }
+        return frameNumber > other.frameNumber; // При равном приоритете - по порядку
+    }
+};
+// Глобальные ресурсы
+priority_queue<VideoFrame> buffer; // Очередь задач с приоритето
+mutex buffer_mtx;                 
+mutex output_mtx;                  
+counting_semaphore<6> cameras_sem(6); // потоки камер
+atomic<int> active_accelerators(3);   // кол-во рабочих ускорителей 
+atomic<bool> running(true);
+
+void accelerator_worker(int id) {
+    while (running || !buffer.empty()) {
+        VideoFrame frame;
+        bool found = false;
+
+        {
+            lock_guard<mutex> lock(buffer_mtx);
+            if (!buffer.empty()) {
+                frame = buffer.top();
+                buffer.pop();
+                found = true;
+            }
+        }
+
+        if (found) {
+            {
+                lock_guard<mutex> lock(output_mtx);
+                cout << "[Ускоритель " << id << "] Обработка: Камера " << frame.cameraId
+                    << ", Кадр " << frame.frameNumber
+                    << (frame.isImportant ? " (ПРИОРИТЕТ)" : " (Обычный)") << endl;
+            }
+            // имитация времени обработки
+            this_thread::sleep_for(chrono::milliseconds(frame.isImportant ? 200 : 500));
+        }
+        else {
+            this_thread::sleep_for(chrono::milliseconds(100));
+        }
+    }
+}
+
+
+void camera_stream(int cameraId) {
+    for (int i = 1; i <= 5; ++i) {
+        cameras_sem.acquire(); 
+
+        VideoFrame frame;
+        frame.cameraId = cameraId;
+        frame.frameNumber = i;
+        frame.isImportant = (i % 2 == 0); // четные кадры 
+
+        {
+            lock_guard<mutex> lock(buffer_mtx);
+            buffer.push(frame); // доба в приоритет очередь
+        }
+
+        {
+            lock_guard<mutex> lock(output_mtx);
+            cout << "Камера " << cameraId << " отправила кадр " << i << endl;
+        }
+
+        cameras_sem.release();
+        this_thread::sleep_for(chrono::milliseconds(300));
+    }
+}
+
+// Глобал мониторинг
+void global_monitoring() {
+    this_thread::sleep_for(chrono::seconds(2));
+    {
+        lock_guard<mutex> lock(output_mtx);
+        cout << "\n!!! МОНИТОРИНГ: Обнаружен отказ Ускорителя №3 !!!" << endl;
+        active_accelerators--; // Перераспределение нагрузки за счет уменьшения потоков 
+        cout << "!!! Задачи перераспределены на оставшиеся " << active_accelerators << " ускорителя.\n" << endl;
+    }
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    // запуск ускорителей ( 3)
+    vector<thread> accels;
+    for (int i = 1; i <= 3; ++i) {
+        accels.push_back(thread(accelerator_worker, i));
+    }
+
+    // запуск потоков камер (6 потоков) 
+    vector<thread> cams;
+    for (int i = 1; i <= 6; ++i) {
+        cams.push_back(thread(camera_stream, i));
+    }
+
+    // запуск мониторинга
+    thread monitor(global_monitoring);
+
+    //завершения работы камер
+    for (auto& t : cams) t.join();
+
+    this_thread::sleep_for(chrono::seconds(3)); // время на доработку хвостов очереди
+    running = false;
+
+    for (auto& t : accels) t.join();
+    monitor.join();
+
+    cout << "Все видеоданные обработаны." << endl;
+    return 0;
+}
